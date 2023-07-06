4b7d7b36b54ce800fa8a5762609ec5adbbf0f20c
ethereal element <vitalik.safonov@list.ru>
2023-07-06 10:47:24 +0300

    init: pagination

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,58 @@
+import { FC } from 'react';
+import styles from "./Pagination.module.scss"
+import {PaginationProps} from "./Pagination.props.ts";
+
+
+
+const paginationButtonArray:string[] = ["Гидроциклы", "Полноприводные", "от 5000", "BRP","еще"];
+
+export const Pagination: FC<PaginationProps> = ({children}:PaginationProps):JSX.Element => {
+    return <section className={styles.pagination}>
+        <div className="container">
+            <h1 className={styles.pagination__title}>{children}</h1>
+
+            <div className={styles.pagination__wrapper}>
+                <ul className={styles.pagination__list}>
+                    {paginationButtonArray.map((item,index) => <li key={index} className={styles.pagination__list_item}>
+                        <button className={styles.pagination__list_item_btn}>{item}</button>
+                    </li>)}
+                </ul>
+
+                <div className={styles.pagination__selection}>
+                    <div className={styles.pagination__selection_arrow}>
+                        <select className={styles.pagination__selection_select}>
+                            <option value="Популярности" className={styles.pagination__selection_select_option}>Популярности</option>
+                            <option value="Цене" className={styles.pagination__selection_select_option}>Цене</option>
+                            <option value="Качеству" className={styles.pagination__selection_select_option}>Качеству</option>
+                        </select>
+
+                    </div>
+                    <div>
+                        <button className={styles.pagination__selection_btn}>
+                            <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="1" y="1" width="21" height="19" stroke="#2F3035" stroke-width="2"/>
+                                <rect x="7" y="6" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
+                                <rect x="7" y="13" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
+                                <rect x="14" y="6" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
+                                <rect x="14" y="13" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
+                            </svg>
+
+                        </button>
+                        <button className={styles.pagination__selection_btn}>
+                            <svg width="25" height="21" viewBox="0 0 25 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g opacity="0.3">
+                                    <rect x="6" width="19" height="3" rx="1.5" fill="#2F3035"/>
+                                    <rect x="6" y="8" width="19" height="3" rx="1.5" fill="#2F3035"/>
+                                    <rect x="6" y="16" width="19" height="3" rx="1.5" fill="#2F3035"/>
+                                    <rect width="3" height="3" rx="1.5" fill="#2F3035"/>
+                                    <rect y="8" width="3" height="3" rx="1.5" fill="#2F3035"/>
+                                    <rect y="16" width="3" height="3" rx="1.5" fill="#2F3035"/>
+                                </g>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>;
+};
--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 import {Header} from "../components/header/Header.tsx";
 import {Footer} from "../components/footer/Footer.tsx";
 import {Breadcrumbs} from "../components/breadcrumbs/Breadcrumbs.tsx";
+import {Pagination} from "../components/pagination/Pagination.tsx";
 
 export const Catalog: FC = () => {
     return <>
     <Header></Header>
         <main>
             <Breadcrumbs></Breadcrumbs>
+            <Pagination>Гидроциклы</Pagination>
         </main>
         <Footer></Footer>
     </>;
