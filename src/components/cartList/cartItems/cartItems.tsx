d9287d8dfd33254df6800fc01f9e8659e8f48077
ethereal element <vitalik.safonov@list.ru>
2023-07-21 17:06:01 +0300

    fix: ui button

--- src/components/cartList/cartItems/cartItems.tsx
+++ src/components/cartList/cartItems/cartItems.tsx
@@ -19,7 +19,7 @@ export const CartItems: FC<CartItemsProps> = ({category, image, title, price}: C
             </div>
             <div className={styles.item__buy}>
                 <Button type={"button"} border={true} size={"small-large"}>Перейти к оплате</Button>
-                <Button type={"button"} border={true} size={"small-large"} remove={"red"}>Удалить</Button>
+                <Button type={"button"} border={true} size={"small-large"} remove={true}>Удалить</Button>
             </div>
         </div>
     </li>;
