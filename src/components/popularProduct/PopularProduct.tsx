73e5baf8c3380fef5034e8ad62e0cfd8ef567852
ethereal element <vitalik.safonov@list.ru>
2023-07-27 20:07:12 +0300

    init

--- src/components/popularProduct/PopularProduct.tsx
+++ src/components/popularProduct/PopularProduct.tsx
@@ -77,9 +77,6 @@ export const PopularProduct: FC<PopularProductProps> = ({ProductData, title, nav
                     }}
                 >
 
-
-
-
                     <ul>
                         {ProductData.map((item, index) => <SwiperSlide
                             className="swiper-slide_product"
