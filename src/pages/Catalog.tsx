532e3e96c1121b4b300b612d7a1ff45941649c56
ethereal element <vitalik.safonov@list.ru>
2023-08-07 22:05:57 +0300

    init: cart item

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -5,18 +5,21 @@ import {Breadcrumbs} from "../components/breadcrumbs/Breadcrumbs.tsx";
 import {Pagination} from "../components/pagination/Pagination.tsx";
 import {ProductContent} from "../components/productContent/productContent.tsx";
 import {catalogData} from "../utils/data/productArray.ts";
+import {CountContext} from "../context/homeContext.ts";
+import {useSelector} from "react-redux";
 
 
 export const Catalog: FC = () => {
-
+    const cart = useSelector(state => state.cart.cart);
     return <>
-
-            <Header></Header>
-            <main>
-                <Breadcrumbs></Breadcrumbs>
-                <Pagination>Гидроциклы</Pagination>
-                <ProductContent ProductData={catalogData}></ProductContent>
-            </main>
-            <Footer></Footer>
+            <CountContext.Provider value={cart}>
+                <Header></Header>
+                <main>
+                    <Breadcrumbs></Breadcrumbs>
+                    <Pagination>Гидроциклы</Pagination>
+                    <ProductContent ProductData={catalogData}></ProductContent>
+                </main>
+                <Footer></Footer>
+            </CountContext.Provider>
     </>;
 };
