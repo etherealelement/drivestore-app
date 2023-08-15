56ffe8fc112a48906f758b4c92a55c1c59b89d25
ethereal element <vitalik.safonov@list.ru>
2023-08-15 16:46:34 +0300

    fix: svg

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -33,11 +33,11 @@ export const Pagination: FC<PaginationProps> = ({children}:PaginationProps):JSX.
                     <div>
                         <button className={styles.pagination__selection_btn}>
                             <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="1" y="1" width="21" height="19" stroke="#2F3035" stroke-width="2"/>
-                                <rect x="7" y="6" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
-                                <rect x="7" y="13" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
-                                <rect x="14" y="6" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
-                                <rect x="14" y="13" width="2" height="2" fill="#2F3035" stroke="#2F3035" stroke-width="2"/>
+                                <rect x="1" y="1" width="21" height="19" stroke="#2F3035" strokeWidth="2"/>
+                                <rect x="7" y="6" width="2" height="2" fill="#2F3035" stroke="#2F3035" strokeWidth="2"/>
+                                <rect x="7" y="13" width="2" height="2" fill="#2F3035" stroke="#2F3035" strokeWidth="2"/>
+                                <rect x="14" y="6" width="2" height="2" fill="#2F3035" stroke="#2F3035" strokeWidth="2"/>
+                                <rect x="14" y="13" width="2" height="2" fill="#2F3035" stroke="#2F3035" strokeWidth="2"/>
                             </svg>
 
                         </button>
