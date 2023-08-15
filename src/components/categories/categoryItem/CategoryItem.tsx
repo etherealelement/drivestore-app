13f34d865a9b41aa2bdd4815172bd7eca6ac75ee
ethereal element <vitalik.safonov@list.ru>
2023-08-15 16:39:50 +0300

    fix: category item

--- src/components/categories/categoryItem/CategoryItem.tsx
+++ src/components/categories/categoryItem/CategoryItem.tsx
@@ -3,17 +3,19 @@ import {CategoryItemProps} from "./CategoryItem.props.ts";
 import styles from "./CategoryItem.module.scss";
 
 export const CategoryItem: FC<CategoryItemProps> = ({image, title, link}:CategoryItemProps):JSX.Element => {
-    return <a href={link} className={styles.categoryItem}>
+    return <>
+        <a href={link} className={styles.categoryItem}>
             <div className={styles.categoryItem__text}>
                 <p className={styles.categoryItem__text_descr}>
                     {title}
                 </p>
-                <a href={link} className={styles.categoryItem__text_more}>
+                <p className={styles.categoryItem__text_more}>
                     Подробее
-                </a>
+                </p>
             </div>
             <div className={styles.categoryItem__image}>
                 <img src={image} alt={title}/>
             </div>
-    </a>;
+        </a>
+    </>;
 };
